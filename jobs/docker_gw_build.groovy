27f959e24f171c6fdfcb411c203bcb0b439e8dce
Sree Pothula <pothula@adobe.com>
2017-02-24 15:23:07 -0800

    test

--- jobs/docker_gw_build.groovy
+++ jobs/docker_gw_build.groovy
@@ -1,27 +1,37 @@
-String basePath = 'Docker Image EXAMPLE'
+String basePath = 'Docker API-Gateway Base Image Build'
 String repo = 'adobe-apis/api-gateway'
 
 folder(basePath) {
-    description 'Docker GW base image'
+    description 'Docker API-Gateway Base Image Build'
 }
 
 job("$basePath/Docker Image Example") {
     scm {
         github repo
     }
+
     triggers {
         scm 'H/5 * * * *'
     }
+
+    logRotator {
+        numToKeep 5
+    }
+
+     parameters {
+        stringParam ('FORCE_TAG', 'gateway-')
+        stringParam ('DOCKER_REPO', 'docker-api-platform-snapshot/apiplatform')
+	stringParam ('GIT_REPO', 'adobe-apis/apigateway')
+        choiceParam('GIT_TYPE', ['PUBLIC', 'PRIVATE'])
+	stringParam 'RELEASE_TAG'
+    }
+
     steps {
         shell readFileFromWorkspace('resources/docker_GW_exec.sh')
         rake ('prepare_fixtures') {
         task ('docker:release')
         rake ('docker:tag')
         installation('ruby-2.3.1')
-        }
-        grails {
-            useWrapper true
-            targets(['test-app', 'war'])
         }
     }
 
@@ -31,7 +41,7 @@ publishers {
                     parameters {
                         currentBuild()
                     }
-                    condition('UNSTABLE_OR_BETTER')
+                    condition('STABLE')
                 }
         }
 }
