2072fa5b76e0719b3c2264d43d607a518870c18a
Sree Pothula <pothula@adobe.com>
2017-02-24 12:11:12 -0800

    added docker groovy script

---
+++ jobs/docker_gw_build.groovy
@@ -0,0 +1,32 @@
+String basePath = 'Docker Image EXAMPLE'
+String repo = 'adobe-apis/api-gateway'
+
+folder(basePath) {
+    description 'Docker GW base image'
+}
+
+job("$basePath/Docker Image Example") {
+    scm {
+        github repo
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+    steps {
+        shell readFileFromWorkspace('resources/docker_GW_exec.sh')
+        rake ('build') target('test')
+        grails {
+            useWrapper true
+            targets(['test-app', 'war'])
+        }
+    }
+}
+
+job("$basePath/grails example deploy") {
+    parameters {
+        stringParam 'host'
+    }
+    steps {
+        shell 'scp war file; restart...'
+    }
+}
